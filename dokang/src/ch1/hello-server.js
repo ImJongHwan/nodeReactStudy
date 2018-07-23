e2b69bdf8ad219bf0a4cabecbe19b0eb83970859
Dongok Kang <k2d2o2@gmail.com>
2018-07-23 21:19:59 +0900

    dokang/src/ch1/hello-server.js

---
+++ dokang/src/ch1/hello-server.js
@@ -0,0 +1,13 @@
+
+const http = require('http')
+
+const svr = http.createServer(handler)
+svr.listen(8081)
+
+function handler(req, res){
+  console.log('url:', req.url)
+  console.log('method:', req.method)
+
+  res.writeHead(200, {'Content-Type': 'text/html'})
+  res.end('<h1>Hello, World!</h1>\n')
+}
