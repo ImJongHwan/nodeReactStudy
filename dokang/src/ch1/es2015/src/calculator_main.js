990018fbe877884a39a0f4fb67d3b15a336c42ae
Dongok Kang <k2d2o2@gmail.com>
2018-07-23 23:19:14 +0900

    dokang/src/ch1/es2015/

---
+++ dokang/src/ch1/es2015/src/calculator_main.js
@@ -0,0 +1,6 @@
+
+
+import {add2, mul2} from "./calculator_module"
+
+console.log('3+5=', add2(3, 5))
+console.log('4*8=', mul2(4, 8))
---
+++ dokang/src/ch1/es2015/src/calculator_module.js
@@ -0,0 +1,8 @@
+
+export function add2(a, b){
+  return a+b
+}
+
+export function mul2(a, b){
+  return a*b
+}
