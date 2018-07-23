d1f4371c69f07922f6a7b01691f70cccf090a343
Dongok Kang <k2d2o2@gmail.com>
2018-07-23 23:08:53 +0900

    dokang/src/ch1/calculator_module.js;calculator_main.js

---
+++ dokang/src/ch1/calculator_main.js
@@ -0,0 +1,6 @@
+
+
+const calculator = require('./calculator_module')
+
+console.log('3+5=', calculator.add1(3, 5))
+console.log('4*8=', calculator.mul1(4, 8))
---
+++ dokang/src/ch1/calculator_module.js
@@ -0,0 +1,15 @@
+
+function add(a, b){
+  return a+b
+}
+
+function mul(a, b){
+  return a*b
+}
+
+module.exports = {
+  'add1': add,
+  'mul1': mul
+}
+
+
