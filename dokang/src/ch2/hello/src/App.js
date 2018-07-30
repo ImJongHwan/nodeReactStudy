25ee23717c69889ff86d7054f59b11cd7b44219b
Dongok Kang <k2d2o2@gmail.com>
2018-07-30 21:44:55 +0900

    dokang ch2 hello build

---
+++ dokang/src/ch2/hello/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return <div className='App'>
+      <h2>달리는 기차 위에 중립은 없다.</h2>
+    </div>
+  }
+
+  // render() {
+  //   return (
+  //     <div className="App">
+  //       <header className="App-header">
+  //         <img src={logo} className="App-logo" alt="logo" />
+  //         <h1 className="App-title">Welcome to React</h1>
+  //       </header>
+  //       <p className="App-intro">
+  //         To get started, edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //     </div>
+  //   );
+  // }
+}
+
+export default App;
