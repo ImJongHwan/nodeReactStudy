2456fb6578e8e75c88eb05b6eb3090767c9e5e69
Dongok Kang <k2d2o2@gmail.com>
2018-08-06 21:45:57 +0900

    dokang ch3 form_simple

---
+++ dokang/src/ch3/form_simple/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { SimpleForm } from "./SimpleForm"
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <SimpleForm />
+      </div>
+    );
+  }
+}
+
+export default App;
