80a978e36208d0ffb3408df9dca675ee956bc656
Dongok Kang <k2d2o2@gmail.com>
2018-08-09 21:47:06 +0900

    dokang ch3 input_compo

---
+++ dokang/src/ch3/input_compo/src/FormInput.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+import logo from './logo.svg';
+import './App.css';
+
+class FormInput extends Component {
+  constructor(props) {
+    super(props)
+    const v = (this.props.value) ? this.props.value : ''
+    this.state = {
+      value: v,
+      isOK: this.checkValue(v)
+    }
+  }
+  checkValue(s) {
+    if (this.props.pattern === null) {
+      return true
+    }
+    return this.props.pattern.test(s)
+  }
+  handleChange(e) {
+    const v = e.target.value
+    const newValue = v.replace(this.props.filter, '')
+    const newIsOK = this.checkValue(newValue)
+    this.setState({
+      value: newValue,
+      isOK: newIsOK
+    })
+  }
+  render() {
+    const msg = this.renderStatusMessage()
+    return (
+      <div className="App">
+        <label>{this.props.label}: <br/>
+          <input type = 'text'
+                 placeholder={this.props.placeholder}
+                 value={this.state.value}
+                 onChange={e => this.handleChange(e)} />
+          {msg}
+        </label>
+      </div>
+    );
+  }
+  renderStatusMessage() {
+    const so = {
+      margin: '8px',
+      padding: '8px',
+      color: 'white'
+    }
+    let msg = null
+    if(this.state.isOK) {
+      so.backgroundColor = 'green'
+      msg = <span style={so}>OK</span>
+    }
+    else {
+      if (this.state.value!=='') {
+        so.backgroundColor = 'red'
+        msg = <span style={so}>NG</span>
+      }
+    }
+    return msg
+  }
+}
+
+FormInput.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  filter: PropTypes.object,
+  pattern: PropTypes.object,
+  value: PropTypes.string,
+  placeholder: PropTypes.string,
+  onChange: PropTypes.func
+}
+
+FormInput.defaultProps = {
+  filter: null,
+  pattern: null,
+  value: '',
+  placeholder: '',
+  onChange: null
+}
+
+export default FormInput;
