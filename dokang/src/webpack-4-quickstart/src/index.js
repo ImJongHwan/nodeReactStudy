4e49e9fb2dbf20d58215bffbda3eb4f2cc7333eb
Dongok Kang <k2d2o2@gmail.com>
2018-08-17 20:51:40 +0900

    dokang redux basic-redux

--- dokang/src/webpack-4-quickstart/src/index.js
+++ dokang/src/webpack-4-quickstart/src/index.js
@@ -1,8 +1,3 @@
-console.log(`I'm a silly entry point`);
+import index from './js/index'
 
-const arr = [1, 2, 3];
-const iAmJavascriptES6 = () => console.log(...arr);
-window.iAmJavascriptES6 = iAmJavascriptES6;
 
-import App from "./App";
-import style from "./main.css";
