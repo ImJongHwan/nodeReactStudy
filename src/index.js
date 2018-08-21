04d7fa876fed7f8b4866934e5986afe174ff3277
Dongok Kang <k2d2o2@gmail.com>
2018-08-21 23:12:17 +0900

    dokang redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import * as ReactDOM from 'react-dom'
+
 console.log(`I'm a silly entry point`);
 
 const arr = [1, 2, 3];
@@ -6,3 +8,12 @@ window.iAmJavascriptES6 = iAmJavascriptES6;
 
 import App from "./App";
 import style from "./main.css";
+import store from './store'
+import React from 'react'
+import {Provider} from 'react-redux'
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app"))
---
+++ src/modules/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+import counter from './counter';
+import post from './post'
+
+export default combineReducers({
+    counter, post
+})
