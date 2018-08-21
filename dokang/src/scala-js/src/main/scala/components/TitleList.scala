339eee740a57d876d75e65ab4227100908bfa497
Dongok Kang <k2d2o2@gmail.com>
2018-08-21 23:11:07 +0900

    refactoring

--- dokang/src/scala-js/src/main/scala/components/TitleList.scala
+++ dokang/src/scala-js/src/main/scala/components/TitleList.scala
@@ -4,15 +4,16 @@ import actions.{AppModel, Article}
 import diode.react.ModelProxy
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
+import store.AppCircuit
 
 object TitleList {
 
-  case class Props(proxy: ModelProxy[List[Article]])
+  case class Props(articles: List[Article])
 
   final class Backend($: BackendScope[Props, Unit]) {
     def render(p: Props): VdomElement ={
       <.ul(^.className:="list-group list-group-flush")(
-        p.proxy().map(article =>
+        p.articles.map(article =>
           <.li(^.className:="list-group-item", ^.key:=article.id) (
             article.title
           )
@@ -24,9 +25,9 @@ object TitleList {
   val Component = ScalaComponent.builder[Props]("TitleList")
     .renderBackend[Backend]
     .build
-  def apply(proxy: ModelProxy[AppModel])= {
-    val connector = proxy.connect(_.articles)
-    connector(p => Component(Props(p)))
+  def apply()= {
+    val connector = AppCircuit.connect(_.articles)
+    connector(p => Component(Props(p.value)))
   }
 }
 
