03e05f036847a52003a44658e2cc57d47be3bc47
Dongok Kang <k2d2o2@gmail.com>
2018-08-22 15:47:40 +0900

    dokang redux-thunk redux-pender

--- dokang/src/redux-thunk/src/modules/post.js
+++ dokang/src/redux-thunk/src/modules/post.js
@@ -1,38 +1,36 @@
 
-import {handleActions} from 'redux-actions'
+import {createAction, handleActions} from 'redux-actions'
 
 import axios from 'axios'
+import {pender} from "redux-pender"
 
 function getPostAPI(postId) {
   return axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
 }
 
 const GET_POST = 'GET_POST'
-const GET_POST_PENDING = 'GET_POST_PENDING'
-const GET_POST_SUCCESS = 'GET_POST_SUCCESS'
-const GET_POST_FAILURE = 'GET_POST_FAILURE '
+// const GET_POST_PENDING = 'GET_POST_PENDING'
+// const GET_POST_SUCCESS = 'GET_POST_SUCCESS'
+// const GET_POST_FAILURE = 'GET_POST_FAILURE '
 
-export const getPost = (postId) => dispatch => {
-  dispatch({type: GET_POST_PENDING})
-  return getPostAPI(postId).then(
-    response => {
-      dispatch({
-        type: GET_POST_SUCCESS,
-        payload: response
-      })
-    }
-  ).catch(error => {
-    dispatch({
-      type: GET_POST_FAILURE,
-      payload: error
-    })
-  })
-}
+// export const getPost = (postId) => dispatch => {
+//   dispatch({type: GET_POST_PENDING})
+//   return getPostAPI(postId).then(
+//     response => {
+//       dispatch({
+//         type: GET_POST_SUCCESS,
+//         payload: response
+//       })
+//     }
+//   ).catch(error => {
+//     dispatch({
+//       type: GET_POST_FAILURE,
+//       payload: error
+//     })
+//   })
+// }
 
-export const getPost2 = postId => ({
-  type: GET_POST,
-  payload: getPostAPI(postId)
-})
+export const getPost2 = createAction(GET_POST, getPostAPI)
 
 
 
@@ -45,29 +43,50 @@ const initialState = {
   }
 }
 
+const initialState2 = {
+  data: {
+    title: '',
+    body: ''
+  }
+}
+
+// export default handleActions({
+//   [GET_POST_PENDING]: (state, action) => {
+//     return {
+//       ...state,
+//       pending: true,
+//       error: false
+//     }
+//   },
+//   [GET_POST_SUCCESS]: (state, action) => {
+//     const {title, body} = action.payload.data
+//     return {
+//       ...state,
+//       pending: false,
+//       data: {
+//         title, body
+//       }
+//     }
+//   },
+//   [GET_POST_FAILURE]: (state, action) => {
+//     return {
+//       ...state,
+//       pending: false,
+//       error: true
+//     }
+//   }
+// }, initialState)
+
 export default handleActions({
-  [GET_POST_PENDING]: (state, action) => {
-    return {
-      ...state,
-      pending: true,
-      error: false
-    }
-  },
-  [GET_POST_SUCCESS]: (state, action) => {
-    const {title, body} = action.payload.data
-    return {
-      ...state,
-      pending: false,
-      data: {
-        title, body
+  ...pender({
+    type: GET_POST,
+    onSuccess: (state, action) => {
+      const {title, body} = action.payload.data
+      return {
+        data: {
+          title, body
+        }
       }
     }
-  },
-  [GET_POST_FAILURE]: (state, action) => {
-    return {
-      ...state,
-      pending: false,
-      error: true
-    }
-  }
-}, initialState)
+  })
+}, initialState2)
--- dokang/src/redux-thunk/src/store.js
+++ dokang/src/redux-thunk/src/store.js
@@ -3,13 +3,10 @@ import modules from './modules'
 import loggerMiddleware from './lib/loggerMiddleware'
 import {createLogger} from 'redux-logger'
 import ReduxThunk from 'redux-thunk'
-import promiseMiddleware from 'redux-promise-middleware'
+import penderMiddleware from 'redux-pender'
 
 const logger = createLogger()
-const customizedPromiseMiddleware = promiseMiddleware({
-  promiseTypeSuffixes: ['PENDING', 'SUCCESS', 'FAILURE']
-})
 
-const store = createStore(modules, applyMiddleware(logger, ReduxThunk, customizedPromiseMiddleware))
+const store = createStore(modules, applyMiddleware(logger, ReduxThunk, penderMiddleware()))
 
 export default store
