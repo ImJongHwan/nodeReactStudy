8f92154b77022e90fe52629a7e82a7d8391d66ed
Dongok Kang <k2d2o2@gmail.com>
2018-08-22 22:09:35 +0900

    dokang ch5 router CustomerApp

--- dokang/src/ch5/router_test/src/index.js
+++ dokang/src/ch5/router_test/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import HelloApp2 from "./HelloApp2"
+import CustomerApp from './CustomerApp'
 
-ReactDOM.render(<HelloApp2 />, document.getElementById('root'));
+ReactDOM.render(<CustomerApp />, document.getElementById('root'));
 registerServiceWorker();
