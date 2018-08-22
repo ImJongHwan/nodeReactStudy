0ae8a19abc4b204e64ef72a72b37108715e515be
Hwan <milkway147@gmail.com>
2018-08-22 22:54:10 +0900

    Finish redux-thunk

---
+++ imjh/redux-starter-kit/src/store.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware } from 'redux'
+import modules from './modules'
+import { createLogger } from 'redux-logger'
+import ReduxThunk from 'redux-thunk'
+
+const logger = createLogger()
+
+const store = createStore(modules, applyMiddleware(logger, ReduxThunk))
+
+export default store
